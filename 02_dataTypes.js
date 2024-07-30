7e0d782a25f4e46c539b4db196cb9afaf8b0c41b
manojraut0803 <manojraut0803@gmail.com>
2024-07-30 13:33:48 +0530

    "use strict" and  data types in js

---
+++ 02_dataTypes.js
@@ -0,0 +1,24 @@
+"use strict";   // this code works the modern way
+
+// alert(2 + 1);   // we are using nodejs, not browser.
+// console.log(2 + 2); console.log(4 + 4);     // Avoid this though it will run [code readability]
+
+let name = "Manoj Raut"
+let age = 21
+let isAlive = true
+let balance = null
+let country = undefined
+/*
+Number  => 2 to power 53
+BigInt
+String  => ""  
+Boolean => true/false
+null    => standalone value
+undefined
+Symbol  => unique
+
+Object
+*/
+console.log(typeof undefined);  // undefined
+console.log(typeof null);       // Object
+
--- README.md
+++ README.md
@@ -2,6 +2,8 @@
 ***Table of contents***
 ```
 variable
+"use strict"
+data types
 
 
 ```
@@ -10,7 +12,22 @@ variable
 * let 
 + const
 
-### Error
+### Errors
 -  TypeError: Assignment to constant variable
 - 
+
+### Data Types
+- Number  => 2 to power 53
+- BigInt
+- String  => ""  
+- Boolean => true/false
+- null    => standalone value
+- undefined
+- Symbol  => unique
+- Object
+```
+>> typeof undefined ==> undefined
+>> typeof null ==> Object
+```
+
 
